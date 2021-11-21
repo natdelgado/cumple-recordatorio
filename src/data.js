f1a2219c48b9f0fc50b648cc1b0e2a71c4046426
Natalia Delgado <carmendelgado1601@gmail.com>
2021-11-21 19:09:10 -0300

    cambie data

--- src/data.js
+++ src/data.js
@@ -7,20 +7,20 @@ export default [
     },
     {
         id:2,
-        name:'Rolo',
-        age: 12,
-        image:"https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg"
+        name:'Firulais',
+        age: 5,
+        image:"https://cdn.pixabay.com/photo/2021/10/19/10/56/cat-6723256_1280.jpg"
     },
     {
         id:3,
-        name:'Rolo',
-        age: 12,
-        image:"https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg"
+        name:'Chuchi',
+        age: 2,
+        image:"https://cdn.pixabay.com/photo/2016/02/10/16/37/cat-1192026_1280.jpg"
     },
     {
         id:4,
-        name:'Rolo',
-        age: 12,
-        image:"https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg"
+        name:'Michi',
+        age: 1,
+        image:"https://cdn.pixabay.com/photo/2021/10/27/19/09/cat-6748193__340.jpg"
     }
 ]
